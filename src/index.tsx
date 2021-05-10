40b482ad29fbe40d26f5f7902f087d476167829e
purkayasta <pritompurkayasta@gmail.com>
2021-05-10 22:20:49 +0600

    Added PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorkerRegistration.register();
